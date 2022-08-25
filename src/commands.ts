39ffd3fb8912f17772f37ca57e1e30055ed44406
suyanlong <592938410@qq.com>
2022-08-25 14:40:15 +0800

    :refactor code

--- src/commands.ts
+++ src/commands.ts
@@ -1,14 +1,31 @@
 'use strict';
 
+import { Downloader } from './downloader';
+import { Compiler } from './compiler';
+import * as vscode from 'vscode';
+
 interface Command {
   command: string;
   title: string;
+  callback: (...args: any[]) => any;
 }
 
 const commands: Command[] = [
   {
-    command: 'format-code.format.ralph',
-    title: 'Format Ralph'
+    command: 'ralph.downloader',
+    title: 'ralph downloader',
+    callback: () => {
+      const download = new Downloader();
+      download.showQuickPick();
+    }
+  },
+  {
+    command: 'ralph.downloader',
+    title: 'ralph downloader',
+    callback: () => {
+      vscode.window.activeTextEditor &&
+        new Compiler().compiler(vscode.window.activeTextEditor);
+    }
   }
 ];
 
--- src/extension.ts
+++ src/extension.ts
@@ -3,10 +3,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-// import commands from "./commands";
+import commands from './commands';
 import { FormatterProvider } from './formatter';
-import { Downloader } from './downloader';
-import { Compiler } from './compiler';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -18,16 +16,12 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  context.subscriptions.push(
-    vscode.commands.registerCommand('ralph.downloader', () => {
-      const download = new Downloader();
-      download.showQuickPick();
-    }),
-    vscode.commands.registerCommand('ralph.compiler', () => {
-      vscode.window.activeTextEditor &&
-        new Compiler().compiler(vscode.window.activeTextEditor);
-    })
+  commands.forEach((value) =>
+    context.subscriptions.push(
+      vscode.commands.registerCommand(value.command, value.callback)
+    )
   );
+
   vscode.languages.registerDocumentFormattingEditProvider(
     { scheme: 'file', language: 'ralph' },
     new FormatterProvider()
