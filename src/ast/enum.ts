3d1525b69c2d5ff6c83dd426d2c06f322baf4c7b
suyanlong <592938410@qq.com>
2022-10-17 15:37:01 +0800

    Add EnumMember

--- src/ast/enum.ts
+++ src/ast/enum.ts
@@ -1,7 +1,10 @@
+// eslint-disable-next-line max-classes-per-file
 import { CompletionItemKind, SymbolKind } from 'vscode'
+import { TerminalNode } from 'antlr4ts/tree/TerminalNode'
 import { Base } from './base'
 import { EnumContext } from '../parser/RalphParser'
-import { Variable } from './variable'
+import { SemanticNode } from './ast'
+import { IdentifierKind } from './identifier'
 
 export class Enum extends Base {
   symbolKind(): SymbolKind {
@@ -15,10 +18,25 @@ export class Enum extends Base {
   public static FromContext(ctx: EnumContext): Enum {
     const enumValue = new Enum(ctx.IDENTIFIER())
     ctx.varNameAssign().forEach((value) => {
-      const member = new Variable(value.varName().IDENTIFIER()).setParent(enumValue)
+      const member = new EnumMember(value.varName().IDENTIFIER()).setParent(enumValue)
       member.detail = value.text
       enumValue.add(member)
     })
     return enumValue
+  }
+}
+
+class EnumMember extends SemanticNode {
+  symbolKind(): SymbolKind {
+    return SymbolKind.EnumMember
+  }
+
+  completionItemKind(): CompletionItemKind {
+    return CompletionItemKind.EnumMember
+  }
+
+  constructor(node: TerminalNode) {
+    super(node)
+    this.identifierKind = IdentifierKind.Variable
   }
 }
