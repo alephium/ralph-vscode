04fa906740db9e0e951a33a89b30f64071971891
suyanlong <592938410@qq.com>
2022-10-20 14:24:28 +0800

    Add method param list

--- src/ast/method.ts
+++ src/ast/method.ts
@@ -1,4 +1,4 @@
-import { CompletionItemKind, SymbolKind } from 'vscode'
+import { CompletionItem, CompletionItemKind, SnippetString, SymbolKind } from 'vscode'
 import { TerminalNode } from 'antlr4ts/tree/TerminalNode'
 import { Base } from './base'
 import { MethodDeclContext } from '../parser/RalphParser'
@@ -14,12 +14,44 @@ export class Method extends Base {
     this.isPub = false
   }
 
+  paramList(): string {
+    let text = ''
+    let { size } = this.members
+    this.members.forEach((value) => {
+      text += `${value.name}: ${value.type_?.name}`
+      size -= 1
+      if (size !== 0) {
+        text += ', '
+      }
+    })
+    return text
+  }
+
   symbolKind(): SymbolKind {
     return SymbolKind.Method
   }
 
   completionItemKind(): CompletionItemKind {
     return CompletionItemKind.Method
+  }
+
+  completionItem(): CompletionItem {
+    const item = new CompletionItem(this.completionItemLabel(), this.completionItemKind())
+    item.preselect = true
+    item.commitCharacters = ['.']
+    const text = new SnippetString(`${this.name}`)
+    text.appendText('(')
+    let { size } = this.members
+    this.members.forEach((value) => {
+      text.appendTabstop().appendPlaceholder(`${value.name}: ${value.type_?.name}`)
+      size -= 1
+      if (size !== 0) {
+        text.appendText(', ')
+      }
+    })
+    text.appendText(')')
+    item.insertText = text
+    return item
   }
 
   label(): string {
