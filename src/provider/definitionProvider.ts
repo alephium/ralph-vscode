2183ca314ae95062fbe3d2fd4f3e9594910a4f7f
suyanlong <592938410@qq.com>
2022-10-21 16:57:30 +0800

    Add definition jump

--- src/provider/definitionProvider.ts
+++ src/provider/definitionProvider.ts
@@ -1,19 +1,9 @@
 import * as vscode from 'vscode'
 import { Definition, DefinitionLink, Position, ProviderResult, TextDocument } from 'vscode'
-import Parser from '../parser/parser'
-import cache from '../cache/cache'
 import { Filter } from './filter'
 
 export class DefinitionProvider extends Filter implements vscode.DefinitionProvider {
   provideDefinition(document: TextDocument, position: Position): ProviderResult<Definition | DefinitionLink[]> {
-    const word = this.word(document, position)
-    if (word) {
-      Parser(document.uri, document.getText())
-      for (const value of cache.values()) {
-        const member = value.def(word)
-        if (member) return member.location?.()
-      }
-    }
-    return undefined
+    return this.callChain(document, position)?.location?.()
   }
 }
