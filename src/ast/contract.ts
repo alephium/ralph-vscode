897b74db3b5571c64de1a82cbc61b1f8e3623fa0
suyanlong <592938410@qq.com>
2022-10-26 18:03:14 +0800

    Add Interface implementation provider

--- src/ast/contract.ts
+++ src/ast/contract.ts
@@ -1,7 +1,20 @@
 import { CompletionItemKind, SymbolKind } from 'vscode'
+import { TerminalNode } from 'antlr4ts/tree/TerminalNode'
 import { Base } from './base'
+import { Interface } from './interface'
 
 export class Contract extends Base {
+  interfaces: Interface | undefined
+
+  parentClass: Contract | undefined
+
+  subclass: Map<string, Contract>
+
+  constructor(node: TerminalNode) {
+    super(node)
+    this.subclass = new Map()
+  }
+
   symbolKind(): SymbolKind {
     return SymbolKind.Class
   }
